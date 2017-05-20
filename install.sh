940a6c1f66a859cafed5504a519d23b6706e12a1
René Fleschenberg <rene@fleschenberg.net>
2017-05-20 12:53:06 +0100

    Add git config

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ dotfiles=$(dirname $(readlink -f $0))
 mkdir ~/.config/nvim || true
 ln -bnfs $dotfiles/vimrc ~/.config/nvim/init.vim
 ln -bnfs $dotfiles/zshrc ~/.zshrc
+ln -bnfs $dotfiles/gitconfig ~/.gitconfig
