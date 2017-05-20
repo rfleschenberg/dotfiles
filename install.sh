c32e74f57496ef3ab36ece8b6ead244afbec54be
René Fleschenberg <rene@fleschenberg.net>
2017-05-20 12:57:42 +0100

    Add i3 configs

--- install.sh
+++ install.sh
@@ -6,5 +6,11 @@ dotfiles=$(dirname $(readlink -f $0))
 
 mkdir ~/.config/nvim || true
 ln -bnfs $dotfiles/vimrc ~/.config/nvim/init.vim
+
 ln -bnfs $dotfiles/zshrc ~/.zshrc
+
 ln -bnfs $dotfiles/gitconfig ~/.gitconfig
+
+mkdir ~/.i3 || true
+ln -bnfs $dotfiles/i3config ~/.i3/config
+ln -bnfs $dotfiles/i3status.conf ~/.i3status.conf
