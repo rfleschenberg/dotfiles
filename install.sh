23bbbd019393601f5e17ae1aa36fee6084c91fa0
René Fleschenberg <rene@fleschenberg.net>
2017-05-21 15:22:28 +0100

    More quoting

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ set -xe
 dotfiles="$(dirname $(readlink -f $0))"
 
 mkdir ~/.config/nvim || true
-ln -bnfs $dotfiles/vimrc ~/.config/nvim/init.vim
+ln -bnfs "$dotfiles/vimrc" ~/.config/nvim/init.vim
 
-ln -bnfs $dotfiles/zshrc ~/.zshrc
+ln -bnfs "$dotfiles/zshrc" ~/.zshrc
 
-ln -bnfs $dotfiles/gitconfig ~/.gitconfig
+ln -bnfs "$dotfiles/gitconfig" ~/.gitconfig
 
 mkdir ~/.i3 || true
-ln -bnfs $dotfiles/i3config ~/.i3/config
-ln -bnfs $dotfiles/i3status.conf ~/.i3status.conf
+ln -bnfs "$dotfiles/i3config" ~/.i3/config
+ln -bnfs "$dotfiles/i3status.conf" ~/.i3status.conf
 
-ln -bnfs $dotfiles/pystartup ~/.pystartup
+ln -bnfs "$dotfiles/pystartup" ~/.pystartup
