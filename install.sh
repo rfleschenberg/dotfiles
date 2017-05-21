fd6e9221afbb43317abc3756fe7eba9863f65bc7
René Fleschenberg <rene@fleschenberg.net>
2017-05-21 16:06:06 +0100

    Make install script silent

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
-set -xe
+set -e
 
 dotfiles="$(dirname $(readlink -f $0))"
 
-mkdir ~/.config/nvim || true
+mkdir ~/.config/nvim 2>/dev/null || true
 ln -bnfs "$dotfiles/vimrc" ~/.config/nvim/init.vim
 
 ln -bnfs "$dotfiles/zshrc" ~/.zshrc
 
 ln -bnfs "$dotfiles/gitconfig" ~/.gitconfig
 
-mkdir ~/.i3 || true
+mkdir ~/.i3 2>/dev/null || true
 ln -bnfs "$dotfiles/i3config" ~/.i3/config
 ln -bnfs "$dotfiles/i3status.conf" ~/.i3status.conf
 
